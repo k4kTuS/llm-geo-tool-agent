a2f20c2f446697fe5e3493f1ac8c4e95f91c2ad4
matus <matus.botek@gmail.com>
2024-11-08 12:26:58 +0100

    Refined system prompt

--- agent.py
+++ agent.py
@@ -21,6 +21,7 @@ You are a helpful assistant working with geographical data. Some questions will 
 These coordinates represent a geographical area on the map. You do not need to ask for the coordinates; allways assume you already know the coordinates of the area you are working with.
 
 Whenever the task involves retrieving information about a location, assume the location is within the area defined by the bounding box coordinates.
+Do not copy the data obtained from the tools directly to the response, but rather summarize it in a way that is easy to understand for the user.
 """
 
 class AgentState(TypedDict):
