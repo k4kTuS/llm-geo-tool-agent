71ed03f86f96735521c62a3a59142174baeb4869
matus <matus.botek@gmail.com>
2024-11-11 19:41:58 +0100

    Moved folium map to custom file, created custom map class

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import time
 
-import folium
-from folium.plugins import Draw
-from folium.utilities import JsCode
 from langchain_core.messages import HumanMessage, SystemMessage
 import streamlit as st
 from streamlit_folium import st_folium
 
 from agent import build_graph, clear_chat_history, SYSTEM_MESSAGE_TEMPLATE
+from drawmap import DrawMap
 from utils import *
 
 st.set_page_config(
@@ -48,38 +46,15 @@ def show_chat_app():
 
         st.subheader("Area of interest")
 
-        # Create the Folium map centered on a predefined area
-        m = folium.Map(location=[49.75, 13.39], zoom_start=13)
-
-        draw = Draw(
-            draw_options={
-                'polyline': False,
-                'polygon': False,
-                'circle': False,
-                'circlemarker': False,
-                'marker': True, # Potentional hotel site
-                'rectangle': True, # Area of interest
-            },
-            edit_options={
-                'edit': True,
-                'remove': True,
-            },
-            # Custom JS that ensures only one rectangle is present at a time on the map
-            on={
-                "add": JsCode(open("assets/js/handleAddDrawing.js", "r").read()),
-                "remove": JsCode(open("assets/js/handleRemoveDrawing.js", "r").read()),
-            }
-        )
-        draw.add_to(m)
-
+        m = DrawMap()
         map_data = st_folium(
-            m,
+            m.map_,
             width=400,
             height=500,
             key="map1",
             center=[49.75, 13.39],
             returned_objects=["all_drawings"],
-            )
+        )
         
         st.session_state["selected_bbox"] = parse_drawing_coords(map_data, "Polygon")
         st.session_state["hotel_site_marker"] = parse_drawing_coords(map_data, "Point")
@@ -109,11 +84,11 @@ def show_chat_app():
                         "hotel_site_marker": hotel_site_marker,
                     },
                     config=config,
-                    stream_mode="values"
+                    stream_mode="values",
                 ):
                     for i in range(last_message_id, len(chunk["messages"])):
                         message = chunk["messages"][i]
-                        write_message(message)   
+                        write_message(message)
                     last_message_id = len(chunk["messages"])
 
     if not st.session_state["chat_prompted"]:
---
+++ drawmap.py
@@ -0,0 +1,30 @@
+import folium
+from folium.plugins import Draw
+from folium.utilities import JsCode
+
+class DrawMap:
+    def __init__(self, location=(49.75, 13.39), zoom_start=13):
+        self.map_ = folium.Map(location=location, zoom_start=zoom_start)
+        self.add_draw()
+    
+    def add_draw(self):
+        draw = Draw(
+            draw_options={
+                'polyline': False,
+                'polygon': False,
+                'circle': False,
+                'circlemarker': False,
+                'marker': True, # Potentional hotel site
+                'rectangle': True, # Area of interest
+            },
+            edit_options={
+                'edit': True,
+                'remove': True,
+            },
+            # Custom JS that ensures only one rectangle is present at a time on the map
+            on={
+                "add": JsCode(open("assets/js/handleAddDrawing.js", "r").read()),
+                "remove": JsCode(open("assets/js/handleRemoveDrawing.js", "r").read()),
+            }
+        )
+        draw.add_to(self.map_)
