7429dc983d95331670cd29e2ae2781ded2514fa3
kaktus <matus.botek@gmail.com>
2025-03-28 21:01:50 +0100

    Moved prompts to single folder

--- agents/geo_agent.py
+++ agents/geo_agent.py
@@ -9,6 +9,7 @@ from langgraph.prebuilt import ToolNode
 from typing_extensions import Annotated, TypedDict, Optional
 import streamlit as st
 
+from agents.prompts import SYSTEM_PROMP_GEO, get_current_timestamp
 from paths import PROJECT_ROOT
 from tools import get_all_tools
 from schemas.geometry import BoundingBox, PointMarker
@@ -16,16 +17,6 @@ from utils.agent_utils import get_chat_history, get_llm
 
 cfg = configparser.ConfigParser()
 cfg.read(f'{PROJECT_ROOT}/config.ini')
-
-SYSTEM_MESSAGE = """
-You are a helpful assistant working with geographical data. Some questions will be tied to an area defined by bounding box coordinates.
-These coordinates represent a geographical area on the map. You do not need to ask for the coordinates; allways assume you already know the coordinates of the area you are working with.
-
-Whenever the task involves retrieving information about a location, assume the location is within the area defined by the bounding box coordinates.
-If you use tools, do not copy the data obtained from the tools directly to the response, but rather summarize it in a way that is easy to understand for the user.
-
-The current timestamp is: {timestamp}
-"""
 
 class AgentState(TypedDict):
     """
@@ -56,8 +47,7 @@ def should_continue(state: AgentState, config: RunnableConfig):
 
 def call_model(state: AgentState, config: RunnableConfig):
     chat_history = get_chat_history()
-    timestamp = datetime.now().strftime("%A, %Y-%m-%d %H:%M:%S")
-    msgs = [SystemMessage(content=SYSTEM_MESSAGE.format(timestamp=timestamp))] + list(chat_history.messages) + state["messages"]
+    msgs = [SystemMessage(content=SYSTEM_PROMP_GEO.format(timestamp=get_current_timestamp()))] + list(chat_history.messages) + state["messages"]
     response = llm_with_tools.invoke(msgs)
     return {"messages": [response]}
 
