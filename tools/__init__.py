286933227d14d17e9ee625087418798d6621e2da
kaktus <matus.botek@gmail.com>
2025-04-06 19:29:42 +0200

    Removed deleted tool import

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,7 +3,7 @@ from .hotel_suitability_tool import HotelSuitabilityTool
 from .land_tools import LandCoverTool, LandUseTool, ElevationTool
 from .openmeteo_tool import CurrentWeatherTool, WeatherForecastTool
 from .spoi_tool import SpoiTool
-from .temperature_tools import TemperatureAnalysisTool, TemperatureLongPredictionTool, TemperatureForecastTool
+from .temperature_tools import TemperatureAnalysisTool, TemperatureLongPredictionTool
 from .tourism_tool import TourismTool
 
 
