f0e7bf32fe7d82e9fdd36ddfa5ab235b292e919a
kaktus <matus.botek@gmail.com>
2025-04-17 16:53:42 +0200

    POI tool refactoring
    - Fixed empty gpd response
    - Added artifact data
    - Handled cases with no relevant categories

--- tools/poi.py
+++ tools/poi.py
@@ -1,5 +1,6 @@
 from typing import Optional, Type
 
+import pandas as pd
 import geopandas as gpd
 import requests
 from langchain_core.tools import BaseTool
@@ -8,6 +9,7 @@ from shapely.geometry import box, shape
 
 from tools.base import GeospatialTool
 from tools.input_schemas.poi import PoiInput
+from schemas.data import DataResponse
 from schemas.geometry import BoundingBox
 from utils.encoder import get_encoder
 from utils.map_service import wfs_config
@@ -22,19 +24,28 @@ class PoiTool(GeospatialTool):
         "It covers the western part of Czech Republic."
     )
     args_schema: Optional[Type[BaseModel]] = PoiInput
+    response_format: str = "content_and_artifact"
     boundary = box(12.4, 48.9, 14.5, 50.15)
 
     def _run(self, bounding_box: BoundingBox, categories: Optional[list[str]] = None):
         gpd_poi = get_poi_data(bounding_box)
         if gpd_poi.empty:
-            return "No points of interest found in the area."
+            return "No points of interest found in the area.", None
         category_counts = gpd_poi.groupby("category").agg(poi_count = ("label", "count")).sort_values(by="poi_count", ascending=False)
         
         if not categories:
-            return (
-                f"Found the following point of interest categories:\n"
+            output =  (
+                f"### Points of interest:\n"
                 + category_counts.to_markdown()
             )
+            data_output = DataResponse(
+                name="Points of Interest",
+                source="OpenStreetMap",
+                data_type="dataframe",
+                data=category_counts.reset_index(),
+                show_data=True,
+            )
+            return output, data_output
         encoder = get_encoder()
         query_categories = [category.lower() for category in categories]
         poi_categories = [category.lower() for category in gpd_poi["category"].unique().tolist()]
@@ -48,10 +59,21 @@ class PoiTool(GeospatialTool):
             if max(similarity[:, i]) > RELEVANCE_THRESHOLD:
                 relevant_poi_cats.append(poi_cat)
         
-        return (
-            "Found the following point of interest categories relevant for the query:\n"
-            + category_counts[category_counts.index.isin(relevant_poi_cats)].to_markdown()
+        res_gdf = category_counts[category_counts.index.isin(relevant_poi_cats)]
+        if res_gdf.empty:
+            return "No relevant points of interest found in the area.", None
+        output = (
+            "### Filtered points of interest:\n"
+            + res_gdf.to_markdown()
         )
+        data_output = DataResponse(
+            name="Points of Interest",
+            source="OpenStreetMap",
+            data_type="dataframe",
+            data=res_gdf.reset_index(),
+            show_data=True,
+        )
+        return output, data_output
     
 def get_poi_data(bounding_box: BoundingBox) -> gpd.GeoDataFrame:
     response = requests.get(
@@ -69,4 +91,6 @@ def get_poi_data(bounding_box: BoundingBox) -> gpd.GeoDataFrame:
         }
         for ftr in data['features']
     ]
+    if not records:
+        return gpd.GeoDataFrame(columns=["label", "category", "geometry"], crs="EPSG:4326")
     return gpd.GeoDataFrame(records, geometry="geometry", crs="EPSG:4326")
