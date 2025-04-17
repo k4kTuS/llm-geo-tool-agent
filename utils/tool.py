cb79eb298de78d680e06b7a271d4865e87127b80
kaktus <matus.botek@gmail.com>
2025-04-17 12:27:33 +0200

    Reworked color counting, added method for counting components

--- utils/tool.py
+++ utils/tool.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import requests
 from scipy.spatial import KDTree
@@ -6,34 +7,47 @@ from schemas.geometry import BoundingBox, PointMarker
 from utils.map_service import *
 
 def get_color_counts(image, rgb_mapping, n_colors=None):
-    pixels = np.array(image)
-    pixels = pixels.reshape(-1, 3)
+    pixels = np.array(image).reshape(-1, 3)
 
+    # Count uniques using byte representation
     pixels_view = pixels.view(np.dtype((np.void, pixels.dtype.itemsize * pixels.shape[1])))
     unique_pixels, counts = np.unique(pixels_view, return_counts=True)
-
-    # Convert back to normal RGB tuples
+    # Convert back to RGB tuples
     unique_pixels = unique_pixels.view(pixels.dtype).reshape(-1, 3)
     # Combine pixels and their counts
-    pixel_counts = zip(map(tuple, unique_pixels), counts)
+    pixel_counts = dict(zip(map(tuple, unique_pixels), counts))
     # Prepare KDTree for color mapping using only directly matched colors
-    matched_colors = [clr for clr in map(tuple, unique_pixels) if clr in list(rgb_mapping.values())]
-
-    rgb_counts = {tuple(rgb): 0 for rgb in matched_colors}
+    matched_colors = [clr for clr in pixel_counts.keys() if clr in list(rgb_mapping.values())]
     kdtree = KDTree(matched_colors)
-    # Get color counts for LU colors
-    for rgb, cnt in pixel_counts:
-        if rgb in rgb_counts:
-            rgb_counts[rgb] += cnt
-        else:
-            # Map missing colors using KDTree and add the count
-            _, index = kdtree.query(rgb)
-            closest_color = tuple(matched_colors[index])
-            rgb_counts[closest_color] += cnt
-    # Sort by counts
-    sorted_pixel_counts = sorted([(k,v) for k,v in rgb_counts.items() if v != 0], reverse=True, key=lambda x: x[1])
+    # Map unmatched colors and increase counts
+    for rgb in set(pixel_counts.keys()) - set(matched_colors):
+        _, index = kdtree.query(rgb)
+        closest_color = tuple(matched_colors[index])
+        pixel_counts[closest_color] += pixel_counts[rgb]
+    # Sort by counts and filter to only matched colors
+    sorted_pixel_counts =  sorted([(k,v) for k,v in pixel_counts.items() if k in matched_colors], reverse=True, key=lambda x: x[1])
     # Get the top n colors
     return sorted_pixel_counts[:n_colors]
+
+def count_components(image_array, colors, connectivity=8):
+    """
+    Counts connected components (zones) of each RGB color using OpenCV.
+    
+    Args:
+        image_array (np.ndarray): The input image as a NumPy array.
+        unique_colors (list): List of unique RGB tuples to count.
+        connectivity (int): Connectivity for connected components (4 or 8).
+    
+    Returns:
+        dict: A dictionary mapping each RGB color to the number of connected components.
+    """
+    zones_per_color = {}
+    for clr in colors:
+        mask = cv2.inRange(image_array, np.array(clr), np.array(clr))
+        num_labels, _ = cv2.connectedComponents(mask, connectivity=connectivity)
+        # Subtract 1 to remove background from count
+        zones_per_color[tuple(clr)] = num_labels - 1
+    return zones_per_color
 
 def find_square_for_marker(square_list, point_marker: PointMarker):
     m_lon = point_marker.x
