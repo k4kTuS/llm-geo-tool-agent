bf231ccfa6591840375d9b3a71abf57edf2fda1a
kaktus <matus.botek@gmail.com>
2025-04-17 14:25:48 +0200

    Return dataresponse for eurostat, fixed one grid cell problem again

--- tools/eurostat.py
+++ tools/eurostat.py
@@ -10,6 +10,7 @@ from typing import Optional, Type
 
 from tools.base import GeospatialTool
 from tools.input_schemas.base import BaseGeomInput
+from schemas.data import DataResponse
 from schemas.geometry import BoundingBox
 from utils.tool import get_map_data
 
@@ -19,6 +20,7 @@ class EurostatPopulationTool(GeospatialTool):
     name: str = "get_eurostat_population_data"
     description: str = "Provides processed population data from eurostat for the bounding box."
     args_schema: Optional[Type[BaseModel]] = BaseGeomInput
+    response_format: str = "content_and_artifact"
     boundary = box(-20.0, 30.0, 40.0, 70.0)
 
     def _run(self, bounding_box: BoundingBox):
@@ -30,10 +32,10 @@ class EurostatPopulationTool(GeospatialTool):
             x_grid_size=EUROSTAT_GRID_SIZE,
             y_grid_size=EUROSTAT_GRID_SIZE
         )
-        # Problem when retrieving only one grid cell - 1x1 pixel is not enough
+        # Problem when retrieving only one grid cell in any direction
         cell_duplication = 1
-        if dimensions[0] == 1 and dimensions[1] == 1:
-            dimensions = (2, 2)
+        if dimensions[0] == 1 or dimensions[1] == 1:
+            dimensions = tuple(d * 2 for d in dimensions)
             cell_duplication = 4
         
         # Download population layers
@@ -62,9 +64,18 @@ class EurostatPopulationTool(GeospatialTool):
         arr_employed = np.array(Image.open(BytesIO(employed_data))) / cell_duplication
         total_sum = int(np.sum(arr_total))
         employed_sum = int(np.sum(arr_employed))
-        return "## Eurostat data:\n"\
+
+        output = "## Eurostat data:\n"\
             + f"- Total population: {total_sum:,}\n"\
-            + f"- Employed population: {employed_sum:,} ({(employed_sum / total_sum+1e-10):.2%})\n"\
+            + f"- Employed population: {employed_sum:,} ({(employed_sum / total_sum+1e-10):.2%})"
+        data_output = DataResponse(
+            name="Eurostat Population Data",
+            source="Eurostat Census 2021",
+            data_type="text",
+            data=output,
+            show_data=True,
+        )
+        return output, data_output
     
 def transform_snap_bbox(bbox: BoundingBox, source_crs: str, target_crs: str, x_grid_size: int, y_grid_size: int):
     """
