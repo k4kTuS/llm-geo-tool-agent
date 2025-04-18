680e1eae952926a6d10b123028fad1bcf562409a
kaktus <matus.botek@gmail.com>
2025-04-18 11:48:15 +0200

    path root fix

--- config/project_paths.py
+++ config/project_paths.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parent
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
 DATA_DIR = PROJECT_ROOT / 'data'
 SAVED_MODELS_DIR = PROJECT_ROOT / 'saved_models'
 RESOURCES_DIR = PROJECT_ROOT / 'resources'
